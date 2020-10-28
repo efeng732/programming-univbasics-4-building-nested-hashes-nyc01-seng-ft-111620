041734ece8acd5f7e6e8cb988e6d6964fb6559f6
Erwin Feng <efeng732@gmail.com>
2020-10-28 22:56:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -38,7 +38,12 @@ monopoly = {
       :four_pieces_owned => 200
       }
     },
-    :names => {}
+    :names => {
+      :reading_railroad => {},
+      :pennsylvania_railroad => {},
+      :b_and_o_railroad => {},
+      :shortline_railroad => {}
+      }
   }
 }
 
